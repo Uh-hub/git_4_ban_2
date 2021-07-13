3314417ae273e676d43f44240f0df179ae7bc646
noa_on_air <tabacal3590@gamil.com>
2021-07-13 11:36:49 +0900

    7/13 gwang in sa 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import NewModel
 
 
@@ -14,7 +16,7 @@ def hello_world(request):
         new_model.text = temp
         new_model.save()
 
-        data_list = NewModel.objects.all()
+        return HttpResponseRedirect(reverse('accoountapp:hello_world'))
 
 
         return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
