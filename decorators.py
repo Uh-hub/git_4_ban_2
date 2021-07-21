1ea13576b5a83d20943bfcccad25bc9e3fa28b52
noa_on_air <tabacal3590@gamil.com>
2021-07-21 18:16:52 +0900

    7/21 gwang in sa 4

--- decorators.py
+++ decorators.py
@@ -11,9 +11,14 @@ def decorator(func):
 @decorator
 
 def hello_world(input_text):
-
     print(input_text)
 
 
 
 hello_world("hello world!")
+
+
+#함수를 인자로 받음 decorator(func)
+#함수 앞 뒤를 꾸며줌 func 앞 뒤 print문
+#인자로 받은 함수 앞뒤
+#감싸진 함수를 다시 리턴
