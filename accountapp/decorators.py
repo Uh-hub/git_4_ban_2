43d713283c60e2ffdd8f3e6c42854d5f742968ab
noa_on_air <tabacal3590@gamil.com>
2021-07-30 11:39:15 +0900

    7/30 gwang in sa dunno

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseForbidden
 
 
 def account_ownership_required(func):
-    def decorated(request,*args, **kwargs):
+    def decorated(request, *args, **kwargs):
         target_user = User.objects.get(pk=kwargs['pk'])
         if target_user == request.user:
             return func(request, *args, **kwargs)
