d6973c8a835352a836c73c68d4471e2111013a34
noa_on_air <tabacal3590@gamil.com>
2021-08-10 11:41:41 +0900

    8/10 gwang in sa 2

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('list/', ArticleListView.as_view(), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', ArticleUpdateView.as_view(), nmae='update'),
-    path('delete/<int:pk>', ArticleDeleteView.as_view(), nmae='delete'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 ]
