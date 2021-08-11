fa4fb158966be9a68b6aab4ab34a6ffd3e98215e
noa_on_air <tabacal3590@gamil.com>
2021-08-11 16:49:31 +0900

    8/11 gwang in sa 1

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 
@@ -9,5 +9,5 @@ app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
-
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
 ]
